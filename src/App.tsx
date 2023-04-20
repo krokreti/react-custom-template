59789fffa393bd826fcfe49904c52aec47fb7748
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-04-20 17:24:38 -0300

    create base of project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NavBar from './components/layout/Navbar';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar>
+        Oi
+      </NavBar>
+    </div>
+  );
+}
+
+export default App;
