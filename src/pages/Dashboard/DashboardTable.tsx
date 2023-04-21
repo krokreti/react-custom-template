79a1503abb4216ba377ca0d8ad506ec961878832
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-04-21 18:56:09 -0300

    Update DashboardTable.tsx

--- src/pages/Dashboard/DashboardTable.tsx
+++ src/pages/Dashboard/DashboardTable.tsx
@@ -1,14 +1,12 @@
-import Table from '@mui/material/Table';
+import * as React from 'react';
 import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import SettingsIcon from '@mui/icons-material/Settings';
-import TablePagination from '@mui/material/TablePagination';
-import { useState } from 'react';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -46,34 +44,15 @@ const rows = [
     createData('Eclair', 262, 16.0, 24, 6.0),
     createData('Cupcake', 305, 3.7, 67, 4.3),
     createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const DashboardTable = () => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
-    return (<Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer>
-            <Table sx={{ minWidth: 650 }} aria-label="a dense table">
+export default function DashboardTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
                 <TableHead>
                     <TableRow>
-                        <StyledTableCell><SettingsIcon /></StyledTableCell>
+                        <StyledTableCell>Dessert (100g serving)</StyledTableCell>
                         <StyledTableCell align="right">Calories</StyledTableCell>
                         <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
                         <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
@@ -81,12 +60,9 @@ const DashboardTable = () => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                        <StyledTableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <StyledTableCell align="left">
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.name}>
+                            <StyledTableCell component="th" scope="row">
                                 {row.name}
                             </StyledTableCell>
                             <StyledTableCell align="right">{row.calories}</StyledTableCell>
@@ -98,17 +74,5 @@ const DashboardTable = () => {
                 </TableBody>
             </Table>
         </TableContainer>
-        <TablePagination
-            rowsPerPageOptions={[5, 10, 50]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-    </Paper>
-    )
+    );
 }
-
-export default DashboardTable;
